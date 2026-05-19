5bda98084f51d842ae7af1e232eb63ee73bf4aa3
Tr.Nghia <t.nghia2112279@gmail.com>
2026-05-19 16:38:43 +0700

    feat(react-foundation): init my-app with Vite and add state demos

--- react/my-app/src/App.tsx
+++ react/my-app/src/App.tsx
@@ -2,21 +2,36 @@ import { useState } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [user, setUser] = useState({
+    id: "12",
+    name: "Nghĩa",
+    address: "117 Tran Hung Dao - Quan 1",
+  });
+  const [animals, setAnimals] = useState(["Dog"]);
+
+  const addAnimal = () => {
+    const list = ["Cat", "Lion", "Tiger"];
+    const random = list[Math.floor(Math.random() * list.length)];
+    setAnimals([...animals, random]);
+  };
+
+  const changeName = () => {
+    setUser({
+      ...user,
+      name: "Nghĩa Dương",
+    });
+  };
 
   return (
-    <>
-      <section id="center">
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-    </>
+    <div>
+      <button onClick={() => setCount(count + 1)}>Count: {count}</button>
+      <p>Animals: {animals.join(", ")}</p>
+      <button onClick={addAnimal}>Add Animal</button>
+      <p>
+        User: {user.name} - {user.address}
+      </p>
+      <button onClick={changeName}>Change Name</button>
+    </div>
   );
 }
 
