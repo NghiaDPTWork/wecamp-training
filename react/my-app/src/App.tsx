f05f40af9fdb2b398d5f4e5de6c47901b1ab0abd
Tr.Nghia <t.nghia2112279@gmail.com>
2026-05-19 16:21:48 +0700

    feat: simplify App layout and apply basic custom styles

---
+++ react/my-app/src/App.tsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <section id="center">
+        <button
+          type="button"
+          className="counter"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          Count is {count}
+        </button>
+      </section>
+
+      <div className="ticks"></div>
+    </>
+  );
+}
+
+export default App;
