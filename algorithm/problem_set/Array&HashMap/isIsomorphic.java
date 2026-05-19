c6b3e7d91b5f61082e96652d9320c52682c50f4a
Tr.Nghia <t.nghia2112279@gmail.com>
2026-05-19 10:53:03 +0700

    refactor(is-isomorphic): update loop condition and formatting in isIsomorphic

--- algorithm/problem_set/Array&HashMap/isIsomorphic.java
+++ algorithm/problem_set/Array&HashMap/isIsomorphic.java
@@ -33,24 +33,23 @@ import java.util.Map;
 
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        if (s.length() != t.length()) return false;
+        if(s.length() != t.length()) return false;
 
         int[] mapS = new int[256];
         int[] mapT = new int[256];
         int len = s.length();
 
-        for (int i = 0; i < len; i++) {
+        for(int i = 0; i <= len - 1; i++){
             char charS = s.charAt(i);
             char charT = t.charAt(i);
 
-            if (mapS[charS] != mapT[charT]) {
+            if(mapS[charS] != mapT[charT]){
                 return false;
             }
 
             mapS[charS] = i + 1;
             mapT[charT] = i + 1;
         }
-
         return true;
     }
 }
