3849a0f49ad01b6d8ef76d291e91ecfee3f73dca
Tr.Nghia <t.nghia2112279@gmail.com>
2026-05-21 09:41:36 +0700

    docs(monotonic): add pros/cons analysis of monotonic check

--- algorithm/problem_set/TwoPointers&Sorting/fixingMonotomic.java
+++ algorithm/problem_set/TwoPointers&Sorting/fixingMonotomic.java
@@ -1,3 +1,19 @@
+/**
+ * Kiểm tra mảng đơn điệu (Monotonic Array).
+ * 
+ * Ưu điểm:
+ * - Đạt hiệu năng tối ưu O(N) nhờ duyệt qua mảng đúng một lần.
+ * - Cơ chế dừng sớm (short-circuiting): Trả về false ngay khi phát hiện cả hai cờ 
+ *   isIncreasing và isDecreasing đều thành false (tức là mảng vừa tăng vừa giảm).
+ * - Sử dụng O(1) không gian bộ nhớ.
+ * 
+ * Nhược điểm:
+ * - Tên file hiện tại bị sai chính tả nhẹ ("Monotomic" thay vì "Monotonic").
+ * 
+ * Độ phức tạp:
+ * - Thời gian: O(N) - trong trường hợp xấu nhất phải duyệt toàn bộ mảng.
+ * - Không gian: O(1).
+ */
 class Solution {
     public boolean isMonotonic(int[] nums) {
         boolean isIncreasing = true;
@@ -18,4 +34,4 @@ class Solution {
 
         return true; 
     }
-}
+}
