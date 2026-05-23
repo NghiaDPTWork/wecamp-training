35a1675700e7fb60348bf0510784ef577dc14086
Tr.Nghia <t.nghia2112279@gmail.com>
2026-05-23 11:05:19 +0700

    feat(algorithm): add path traversal and coordinate checking problem description

---
+++ algorithm/topic.java
@@ -0,0 +1,9 @@
+// cho các kí hiệu lên, xuống, trái, phải 
+// hãy check true flase sao cho khi đi theo string mẫu được cho
+// thì có đi hết từ đầu đến đích hay không 
+/*
+    Nhóm tình huống:
+    1. Đi lên, xuống, trái, phải
+*/
+
+// Đề 2: 
